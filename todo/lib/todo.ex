f873783bc2a46e69b2f76fae9b055dc30460890b
Harrisonp365 <harrisonpowell365@gmail.com>
2021-11-14 16:58:41 +1100

    trying IO and different types

--- todo/lib/todo.ex
+++ todo/lib/todo.ex
@@ -2,4 +2,12 @@ defmodule Todo do
   def hello_world do
     "Hello world"
   end
+  
+  def hello_world2 do
+    IO.puts("Hello world using IO.puts")
+  end
+  # creating a list 
+  def my_list do
+    ["item1", "item2", "item3"]
+  end
 end
