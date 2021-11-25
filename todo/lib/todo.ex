25e3b730276f5f2ace3157ab536b4c58a329e590
Harrisonp365 <harrisonpowell365@gmail.com>
2021-11-25 14:53:05 +1100

    full basic little todo app

--- todo/lib/todo.ex
+++ todo/lib/todo.ex
@@ -1,13 +1,39 @@
 defmodule Todo do
-  def hello_world do
-    "Hello world"
+  def create_todos do
+    {number_of_tasks, _} =
+      IO.gets("Enter the number of tasks you want to add: ") |> Integer.parse()
+
+    for _ <- 1..number_of_tasks do
+      IO.gets("Enter Task: ") |> String.trim()
+    end
   end
-  
-  def hello_world2 do
-    IO.puts("Hello world using IO.puts")
+
+  def contains?(tasks, task) do
+    Enum.member?(tasks, task)
   end
-  # creating a list 
-  def my_list do
-    ["item1", "item2", "item3"]
+
+  def complete_task(tasks, task) do
+    if contains?(tasks, task) do
+      List.delete(tasks, task)
+    else
+      "task does not exist"
+    end
+  end
+
+  def random_task(tasks) do
+    [task] = Enum.take_random(tasks, 1)
+    task
+  end
+
+  def save(tasks, filename) do
+    binary = :erlang.term_to_binary(tasks)
+    File.write(filename, binary)
+  end
+
+  def read(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "The file does not exist"
+    end
   end
 end
